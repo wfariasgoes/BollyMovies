52a04e2296e81e30ff50cafdae117f43722003cb
Wesley Goes <wfariasgoes@gmail.com>
2017-05-14 23:19:36 -0300

    Filme detalhe activity

---
+++ BollyFilmes/app/src/main/java/br/com/bollyfilmes/view/activitys/FilmeDetalheActivity.java
@@ -0,0 +1,15 @@
+package br.com.bollyfilmes.view.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.bollyfilmes.R;
+
+public class FilmeDetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filme_detalhe);
+    }
+}
